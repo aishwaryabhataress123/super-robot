380da270f251df7d6b6566e18cbf5670700eca0e
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-07-23 14:03:07 +0530

    Update lead.php

--- lead.php
+++ lead.php
@@ -14,22 +14,26 @@ $name = $company = $leadcurrency = $leadstatus = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 if (empty($_POST["name"])) {
-  $nameErr = "Name is required";
+$nameErr = "Name is required";
 } else {
-  $name = test_input($_POST["name"]);
-  // check if name only contains letters and whitespace
-  if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-    $nameErr = "Only letters and white space allowed"; 
-  }
+$name = test_input($_POST["name"]);
+// check if name only contains letters and whitespace
+if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+  $nameErr = "Only letters and white space allowed"; 
+}
 }
 if (empty($_POST["company"])) {
-  $companyErr = "Company is required";
+$companyErr = "Company is required";
 } else {
-  $company = test_input($_POST["company"]);
-  // check if name only contains letters and whitespace
-  if (!preg_match("/^[a-zA-Z ]*$/",$company)) {
-    $companyErr = "Only letters and white space allowed"; 
-  }
+$company = test_input($_POST["company"]);
+// check if name only contains letters and whitespace
+if (!preg_match("/^[a-zA-Z ]*$/",$company)) {
+  $companyErr = "Only letters and white space allowed"; 
+}
+}
+  if($_POST['submit'])
+{
+$leadcurrency=$_POST['leadcurrency'];
 }
 }
 
@@ -44,40 +48,36 @@ return $data;
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
 <table>
-  <tr>
-    <td> Name:</td>
-    <td> <input type="text" name="name" value="<?php echo $name;?>"></td>
-    <td><span class="error">* <?php echo $nameErr;?></span></td>
-  </tr>
-  <tr>
-    <td>Company:</td> 
-    <td><input type="text" name="company" value="<?php echo $company;?>"></td>
-    <td><span class="error">* <?php echo $companyErr;?></span></td>
-  </tr>
-  <tr>
-    <td>Lead Currency :</td> 
-    <td>
-      <select name="leadcurrency" id="leadcurrency">
-        <option value="u.s.dollar">U.S. Dollar</option>
-        <option value="euro">Euro</option>
-      </select>
-    </td>
-  </tr>
-  if($_POST['submit'])
-{
-$leadcurrency=$_POST['leadcurrency'];
-}
-  <tr>
-    <td>Lead Status :</td> 
-    <td>
-      <select name="lead status" value="<?php echo $leadstatus;?>">
-        <option value="open">Open</option>
-        <option value="contacted">Contacted</option>
-        <option value="qualified">Qualified</option>
-        <option value="unqualified">Unqualified</option>
-      </select>
-    </td>
-  </tr>
+<tr>
+  <td> Name:</td>
+  <td> <input type="text" name="name" value="<?php echo $name;?>"></td>
+  <td><span class="error">* <?php echo $nameErr;?></span></td>
+</tr>
+<tr>
+  <td>Company:</td> 
+  <td><input type="text" name="company" value="<?php echo $company;?>"></td>
+  <td><span class="error">* <?php echo $companyErr;?></span></td>
+</tr>
+<tr>
+  <td>Lead Currency :</td> 
+  <td>
+    <select name="leadcurrency" id="leadcurrency">
+      <option value="u.s.dollar">U.S. Dollar</option>
+      <option value="euro">Euro</option>
+    </select>
+  </td>
+</tr>
+<tr>
+  <td>Lead Status :</td> 
+  <td>
+    <select name="lead status" value="<?php echo $leadstatus;?>">
+      <option value="open">Open</option>
+      <option value="contacted">Contacted</option>
+      <option value="qualified">Qualified</option>
+      <option value="unqualified">Unqualified</option>
+    </select>
+  </td>
+</tr>
 </table>
 <input type="submit" name="submit" value="Submit">  
 </form>
@@ -96,6 +96,6 @@ echo $leadcurrency;
 echo "Lead Status :"
 echo $leadstatus;
 ?>
-  
+
 </body>
 </html>
