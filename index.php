bfa05e5e5f8aa26d1b5a1c7f3755e593014e634a
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-07-24 18:47:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <script type="text/javascript">
     function checkstate()
     {
-       document.write(<?php echo $state; ?>);
+       alert('<?php echo $state; ?>');
       if($_POST[state]=="Maharashtra" ||$_POST[state]=="maharashtra" )
       {
         alert ("You selected Maharashtra as state");
